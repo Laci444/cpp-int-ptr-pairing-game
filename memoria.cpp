8c0b41b57fb28889ea5b59973646420236287501
Laszlo Szilagyi <29151964+Laci444@users.noreply.github.com>
2024-03-07 22:10:41 +0100

    code refactores

--- memoria.cpp
+++ memoria.cpp
@@ -1,73 +1,81 @@
 #include <algorithm>
-#include <array>
 #include <functional>
 #include <iostream>
 #include <random>
+#include <regex>
 #include <utility>
 
-using namespace std;
-
-auto createGenerator(mt19937 &mt, int lower_bound, int upper_bound) {
-  uniform_int_distribution<int> dist(lower_bound, upper_bound);
-  return bind(dist, mt);
+auto createGenerator(const std::mt19937 &mt, int lower_bound, int upper_bound) {
+  std::uniform_int_distribution<int> dist(lower_bound, upper_bound);
+  return std::bind(dist, mt);
 }
 
 int getUserInput() {
-  cout << "Add meg, mennyi int-pointer part szeretnel! ";
+  std::cout << "Add meg, mennyi int-pointer part szeretnel! ";
   int numberOfPairs;
-  cin >> numberOfPairs;
+  std::cin >> numberOfPairs;
 
-  while (!cin) {
-    cout << "Egy pozitiv egesz szamot adj meg! ";
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cin >> numberOfPairs;
+  while (!std::cin) {
+    std::cout << "Egy pozitiv egesz szamot adj meg! ";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin >> numberOfPairs;
   }
 
   return numberOfPairs;
 }
 
 void writeToScreen(int *numbers, int **pointers, int length) {
-  cout << "Ezek a szamok maradtak: " << endl;
+  std::cout << "Ezek a szamok maradtak: " << std::endl;
   for (int i = 0; i < length; i++) {
     if (numbers[i] == 0)
       continue;
-    cout << i + 1 << ": " << numbers[i] << endl;
+    std::cout << i + 1 << ": " << numbers[i] << std::endl;
   }
-  cout << "Ezek pedig a pointerek:" << endl;
+  std::cout << "Ezek pedig a pointerek:" << std::endl;
   for (int i = 0; i < length; i++) {
     if (pointers[i] == NULL)
       continue;
-    cout << i + 1 << ": " << pointers[i] << endl;
+    std::cout << i + 1 << ": " << pointers[i] << std::endl;
   }
-  cout << "Tippelj egy part (szam:szam): ";
+  std::cout << "Tippelj egy part (szam:szam): ";
 }
 
-pair<int, int> getGuess() {
-  string input;
-  cin >> input;
+std::pair<int, int> getGuess(int maxGuess) {
+  std::string input;
+  std::regex reg("^[0-9]*:[0-9]*$");
+  int colonIndex, numberGuess, pointerGuess;
+  while (true) {
+    std::cin >> input;
 
-  while (!cin) {
-    cout << "A szintaxis: szam:szam! ";
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cin >> input;
+    if (!std::cin) {
+      std::cout << "A szintaxis: szam:szam! ";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+
+    if (!std::regex_match(input, reg)) {
+      std::cout << "A szintaxis: szam:szam! ";
+      continue;
+    }
+
+    colonIndex = input.find_first_of(':');
+    numberGuess = std::stoi(input.substr(0, colonIndex));
+    pointerGuess = std::stoi(input.substr(colonIndex + 1, input.size()));
+
+    if (numberGuess > maxGuess || pointerGuess > maxGuess) {
+      std::cout << "A legnagyobb lehetseges bemenet " << maxGuess
+                << ". Probald ujra! " << std::endl;
+      continue;
+    }
+    return std::pair(numberGuess, pointerGuess);
   }
-
-  // oh god no i wont validate this input
-  // after half an our googling i dont want to do this
-  // lets just pretend every input is perfect
-
-  int colonIndex = input.find_first_of(':');
-  int numberGess = stoi(input.substr(0, colonIndex));
-  int pointerGess = stoi(input.substr(colonIndex + 1, input.size()));
-
-  return pair(numberGess, pointerGess);
 }
 
 int main() {
-  random_device rd;
-  mt19937 mt(rd());
+  std::random_device rd;
+  std::mt19937 mt(rd());
   // create pseudo random number generator that yields from 1 to 100
   auto generator = createGenerator(mt, 1, 100);
   // get user imput on how many number-pointer pairs they want to generate
@@ -80,30 +88,30 @@ int main() {
     pointers[i] = &numbers[i];
   }
 
-  shuffle(pointers, pointers + numberOfPairs, mt);
+  std::shuffle(pointers, pointers + numberOfPairs, mt);
 
   int counter = 0;
   // game loop
   while (true) {
     writeToScreen(numbers, pointers, numberOfPairs);
 
-    pair<int, int> guess = getGuess();
+    std::pair<int, int> guess = getGuess(numberOfPairs);
 
-    cout << "A tipped (" << guess.first << " : " << guess.second << ") ";
+    std::cout << "A tipped (" << guess.first << " : " << guess.second << ") ";
 
     if (&numbers[guess.first - 1] == pointers[guess.second - 1]) {
-      cout << "HELYES!!" << endl;
+      std::cout << "HELYES!!" << std::endl;
       numbers[guess.first - 1] = 0;
       pointers[guess.second - 1] = NULL;
       counter++;
     } else {
-      cout << "sajnos helytelen :(" << endl;
+      std::cout << "sajnos helytelen :(" << std::endl;
     }
     if (counter >= numberOfPairs)
       break;
   }
 
-  cout << "Gratulalok! Sikeresen eltalaltad az osszes part!" << endl;
+  std::cout << "Gratulalok! Sikeresen eltalaltad az osszes part!" << std::endl;
 
   return 0;
 }
